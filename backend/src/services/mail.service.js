6ab17483eb790fa6ffa6ad51c6733342158132a6
Prince Tripathi <prince45tri@gmail.com>
2026-07-12 01:42:58 +0530

    fix: update nodemailer configuration and clean up commented code

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -3,7 +3,9 @@ import nodemailer from 'nodemailer';
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     // type: "OAuth2",
     user: process.env.GOOGLE_USER,
@@ -13,20 +15,16 @@ const transporter = nodemailer.createTransport({
     pass: process.env.GOOGLE_APP_PASSWORD,
   },
 });
-console.log("GOOGLE_USER:", process.env.GOOGLE_USER);
-console.log(
-  "GOOGLE_APP_PASSWORD:",
-  process.env.GOOGLE_APP_PASSWORD ? "FOUND" : "MISSING",
-);
 
 
-transporter.verify()
-.then(() => {
-  console.log("Email transporter is ready");
-})
-.catch((err) => {
-  console.error("Error setting up email transporter", err);
-});
+
+// transporter.verify()
+// .then(() => {
+//   console.log("Email transporter is ready");
+// })
+// .catch((err) => {
+//   console.error("Error setting up email transporter", err);
+// });
 
 export async function sendEmail({to, subject, html, text}) {
   const mailOptions = {
